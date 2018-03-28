9b11a6bc68e4ee171b66dd2e8ed796082df17cc0
Rob Sewell <mrrobsewell@outlook.com>
2018-03-28 13:55:55 +0100

    added install ssms and pester tests

---
+++ Install-DbaSSMS.Tests.ps1
@@ -0,0 +1,162 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Install-DbaSSMS" {
+    Mock Invoke-WebRequest {}
+    Mock Write-Warning {}
+    Mock Test-Path {$true}
+    function Start-DbaFileDownload {}
+    Mock Start-DbaFileDownload {}
+    Mock Get-Process {}
+    Mock Stop-Process {}
+    Mock Start-Process {}
+    Context "Input" {
+        It "Should have a parameter of URL" {
+            (Get-Command Install-DbaSSMS).Parameters['URL'].Count | Should -Be 1 -Because "We may want to change it"
+        }
+        It "URL Parameter should not be mandatory" {
+            (Get-Command Install-DbaSSMS).Parameters['URL'].Attributes.Mandatory | Should -BeFalse -Because "We generally want to use the default value"
+        }
+        It "URL Parameter should be of type string" {
+            (Get-Command Install-DbaSSMS).Parameters['URL'].ParameterType.Name | Should -Be 'String' -Because "What else would an URL be?"
+        }
+        It "Should have a parameter of DownloadPath" {
+            (Get-Command Install-DbaSSMS).Parameters['DownloadPath'].Count | Should -Be 1 -Because "We may need to set a folder we want to download"
+        }
+        It "DownloadPath Parameter should not be mandatory" {
+            (Get-Command Install-DbaSSMS).Parameters['DownloadPath'].Attributes.Mandatory | Should -BeFalse -Because "We generally want to use the default value"
+        }
+        It "DownloadPath Parameter should be of type string" {
+            (Get-Command Install-DbaSSMS).Parameters['DownloadPath'].ParameterType.Name | Should -Be 'String' -Because "What else would an URL be?"
+        }
+        It "Should throw if no access to Download Path if it is specified" {
+            Mock Test-Path {$false}
+
+            { Install-DbaSSMS -DownloadPath 'madeup'} | Should -Throw -Because "We need to be able to access the path"
+
+            $assertMockParams = @{
+                'CommandName' = 'Test-Path'
+                'Times'       = 1
+                'Exactly'     = $true
+            }
+            Assert-MockCalled @assertMockParams
+        }
+        It "Should write a warning if no access to default Download Path" {
+            Mock Test-Path {$false}
+
+            Install-DbaSSMS 
+
+            $assertMockParams = @{
+                'CommandName' = 'Test-Path'
+                'Times'       = 1
+                'Exactly'     = $true
+                'Scope'       = 'It'
+            }
+            Assert-MockCalled @assertMockParams
+
+            $assertMockParams = @{
+                'CommandName' = 'Write-Warning'
+                'Times'       = 1
+                'Exactly'     = $true
+                'Scope'       = 'It'
+            }
+            Assert-MockCalled @assertMockParams
+        }
+        It "Should have a parameter of Upgrade" {
+            (Get-Command Install-DbaSSMS).Parameters['Upgrade'].Count | Should -Be 1 -Because "We want to enable upgrades"
+        }
+        It "Upgrade Parameter should not be mandatory" {
+            (Get-Command Install-DbaSSMS).Parameters['Upgradeh'].Attributes.Mandatory | Should -BeFalse -Because "The default should be full install"
+        }
+        It "Upgrade Parameter should be of type Switch" {
+            (Get-Command Install-DbaSSMS).Parameters['Upgrade'].ParameterType.Name | Should -Be 'SwitchParameter' -Because "It only needs to be a switch"
+        }
+    }
+    Context "Execution" {
+        It "Should Not Throw" {
+            { Install-dbassms }| Should -Not -Throw
+        }
+
+        Install-DbaSSMS
+
+        It "Should get the download link from the web-site" {
+            $assertMockParams = @{
+                'CommandName' = 'Invoke-WebRequest'
+                'Times'       = 2
+                'Exactly'     = $true
+            }
+            Assert-MockCalled @assertMockParams
+        }
+
+        It "Should Download the file" {
+            $assertMockParams = @{
+                'CommandName' = 'Start-DbaFileDownload'
+                'Times'       = 2
+                'Exactly'     = $true
+            }
+            Assert-MockCalled @assertMockParams
+        }
+
+        It "Should run the install fiile"{
+            $assertMockParams = @{
+                'CommandName' = 'Start-Process'
+                'Times'       = 2
+                'Exactly'     = $true
+            }
+            Assert-MockCalled @assertMockParams
+        }
+        It "Should close SSMS if it is running" {
+            # Just a thing to pass the if
+            Mock Get-Process {'a thing'}
+            Install-DbaSSMS
+            $assertMockParams = @{
+                'CommandName' = 'Stop-Process'
+                'Times'       = 1
+                'Exactly'     = $true
+                'Scope' = 'It'
+            }
+            Assert-MockCalled @assertMockParams
+        }
+        It "Should Write a warning if it errors installing" {
+            Mock Start-Process {Throw} 
+            Install-DbaSSMS
+            $assertMockParams = @{
+                'CommandName' = 'Write-Warning'
+                'Times'       = 1
+                'Exactly'     = $true
+                'Scope'       = 'It'
+            }
+            Assert-MockCalled @assertMockParams
+        }
+
+        It "Should Stop if it fails to get download link" {
+            Mock Invoke-WebRequest {Throw}
+            Install-DbaSSMS
+            $assertMockParams = @{
+                'CommandName' = 'Write-Warning'
+                'Times'       = 1
+                'Exactly'     = $true
+                'Scope'       = 'It'
+            }
+            Assert-MockCalled @assertMockParams
+        }
+        Mock Invoke-WebRequest {}
+
+        It "Should Write a warning if it errors downloading the file" {
+            Mock Start-DbaFileDownload {Throw} 
+            Install-DbaSSMS
+            $assertMockParams = @{
+                'CommandName' = 'Write-Warning'
+                'Times'       = 1
+                'Exactly'     = $true
+                'Scope'       = 'It'
+            }
+            Assert-MockCalled @assertMockParams
+        }
+    }
+    
+    Context "Output" {
+
+    }
+}
---
+++ Install-DbaSSMS.ps1
@@ -0,0 +1,100 @@
+﻿function Install-DbaSSMS {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    Param(
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string] $URL = "https://msdn.microsoft.com/en-us/library/mt238290.aspx",
+        [Parameter(Mandatory = $false)]
+        [ValidateScript( {Test-Path $_})]
+        [ValidateNotNullOrEmpty()]
+        [string] $DownloadPath = "$ENV:USERPROFILE\Downloads",
+        [Parameter(Mandatory = $false)]
+        [switch]$Upgrade
+    )
+
+    #region SanityCheck
+    if ( -not (Test-Path $DownloadPath)) {
+        Write-Warning "I can't access $DownloadPath -  won't be able to continue. Please specify a directory this account has access to"
+        Return
+    }
+    #endregion
+    function Start-DbaFileDownload {
+        Param(
+            $url,
+            $OutputFile
+        )
+        (New-Object System.Net.WebClient).DownloadFile($url, $outputFile)
+    }
+
+    #region Get the download link
+    # Go to the Microsoft Site and grab the download link
+
+    try {
+        $pscmdlet.WriteVerbose("Getting the download link from $url")
+        $webpage = Invoke-WebRequest -Uri $url -UseBasicParsing 
+        ## Choose the link depending on if it is an ugrade or a full install
+        if ($Upgrade) {
+            $pscmdlet.WriteVerbose("Getting the download link for upgrade")
+            $Downloadlink = $webpage.Links.Where{$_.OuterHtml -match 'Download SQL Server Management'}[1].href
+            $InstallFile = "$DownloadPath\Autoinstall-SSMS-Setup-ENU-Upgrade.exe"
+        }
+        else {
+            $pscmdlet.WriteVerbose("Getting the download link for full install")
+            $Downloadlink = $webpage.Links.Where{$_.OuterHtml -match 'Download SQL Server Management'}[0].href
+            $InstallFile = "$DownloadPath\Autoinstall-SSMS-Setup-ENU.exe"
+        }
+        $pscmdlet.WriteVerbose("Got the download link $Downloadlink")
+    }
+    Catch {
+        Write-Warning "Failed To Get Download link from $url"
+        Return
+    }
+    #endregion
+
+    #region Download the file
+    try {
+        $pscmdlet.WriteVerbose("Starting Downloading the file from $DownloadLink to $InstallFile")
+        Start-DbaFileDownload -Url $DownloadLink -OutputFile $InstallFile 
+        $pscmdlet.WriteVerbose("Downloaded the file to $InstallFile ")
+    }
+    catch {
+        Write-Warning "Failed To Get Download link from $url"
+        Return
+    }
+    #endregion
+
+    #region Stop SSMS if it is running
+
+    if (Get-Process SSMS -ErrorAction SilentlyContinue) {
+        if ($PSCmdlet.ShouldProcess("$ENV:COMPUTERNAME", "Stopping SSMS process on ")) {
+            try {
+                Stop-Process -Name SSMS
+                $pscmdlet.WriteVerbose("Stopped SSMS with a big bang - hope nothing was running")
+            }
+            catch {
+                Write-Warning "Failed To stop SSMS - Need to bomb out as it wont install with SSMS running"
+                Return
+            }
+        }
+    }
+    else {
+        $pscmdlet.WriteVerbose("SSMS Not Running - Carry On")
+    }
+    #endregion
+
+    #region Install SSMS
+
+    if ($PSCmdlet.ShouldProcess("$ENV:COMPUTERNAME", "Installing SSMS on ")) {
+        try {
+            Start-Process -FilePath $InstallFile -ArgumentList "/install /quiet /norestart" -Wait
+            $pscmdlet.WriteVerbose("SSMS installed or upgraded")
+        }
+        catch {
+            Write-Warning "Failed to install or upgrade SSMS"
+        }
+    }
+    #endregion
+}
+
+
+
