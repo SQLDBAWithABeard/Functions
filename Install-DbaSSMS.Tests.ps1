3790912137dbba096cea6942f174d33c5c3a35f9
Rob Sewell <mrrobsewell@outlook.com>
2018-03-28 15:09:33 +0100

    added tests for help and script analyser

--- Install-DbaSSMS.Tests.ps1
+++ Install-DbaSSMS.Tests.ps1
@@ -116,7 +116,7 @@ Describe "Install-DbaSSMS" {
             Assert-MockCalled @assertMockParams
         }
 
-        It "Should run the install fiile"{
+        It "Should run the install fiile" {
             $assertMockParams = @{
                 'CommandName' = 'Start-Process'
                 'Times'       = 2
@@ -132,7 +132,7 @@ Describe "Install-DbaSSMS" {
                 'CommandName' = 'Stop-Process'
                 'Times'       = 1
                 'Exactly'     = $true
-                'Scope' = 'It'
+                'Scope'       = 'It'
             }
             Assert-MockCalled @assertMockParams
         }
@@ -189,5 +189,98 @@ Describe "Install-DbaSSMS" {
     
     Context "Output" {
 
+    }
+
+    ## Add Script Analyser Rules
+    Context "Testing Install-DbaSSMS for Script Analyser" {
+        $Rules = Get-ScriptAnalyzerRule 
+        $Name = $sut.Split('.')[0]
+        foreach ($rule in $rules) { 
+            $i = $rules.IndexOf($rule)
+            It "passes the PSScriptAnalyzer Rule number $i - $rule  " {
+                (Invoke-ScriptAnalyzer -Path "$here\$sut" -IncludeRule $rule.RuleName ).Count | Should Be 0 
+            }
+        }
+    }
+
+    ##            	
+    ## 	.NOTES
+    ## 		===========================================================================
+    ## 		Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.119
+    ## 		Created on:   	4/12/2016 1:11 PM
+    ## 		Created by:   	June Blender
+    ## 		Organization: 	SAPIEN Technologies, Inc
+    ## 		Filename:		*.Help.Tests.ps1
+    ## 		===========================================================================
+    ## 	.DESCRIPTION
+    ## 	To test help for the commands in a module, place this file in the module folder.
+    ## 	To test any module from any path, use https://github.com/juneb/PesterTDD/Module.Help.Tests.ps1
+    ## 
+    ##     ## ALTERED FOR ONE COMMAND - Rob Sewell 10/05/2017
+    ## 
+    $commandName = 'Install-DbaSSMS'
+    Describe "Test help for $commandName" {
+        # The module-qualified command fails on Microsoft.PowerShell.Archive cmdlets
+        $Help = Get-Help $commandName -ErrorAction SilentlyContinue
+        # If help is not found, synopsis in auto-generated help is the syntax diagram
+        It "should not be auto-generated" {
+            $Help.Synopsis | Should Not BeLike '*`[`<CommonParameters`>`]*'
+        }
+    
+        # Should be a description for every function
+        It "gets description for $commandName" {
+            $Help.Description | Should Not BeNullOrEmpty
+        }
+    
+        # Should be at least one example
+        It "gets example code from $commandName" {
+            ($Help.Examples.Example | Select-Object -First 1).Code | Should Not BeNullOrEmpty
+        }
+    
+        # Should be at least one example description
+        It "gets example help from $commandName" {
+            ($Help.Examples.Example.Remarks | Select-Object -First 1).Text | Should Not BeNullOrEmpty
+        }
+    
+        Context "Test parameter help for $commandName" {
+            $command = Get-Command $CommandName
+            $Common = 'Debug', 'ErrorAction', 'ErrorVariable', 'InformationAction', 'InformationVariable', 'OutBuffer', 'OutVariable',
+            'PipelineVariable', 'Verbose', 'WarningAction', 'WarningVariable', 'Confirm', 'Whatif'
+        
+            $parameters = $command.ParameterSets.Parameters | Sort-Object -Property Name -Unique | Where-Object Name -notin $common
+            $parameterNames = $parameters.Name
+            $HelpParameterNames = $Help.Parameters.Parameter.Where{$_.Name -notin $common}.Name | Sort-Object -Unique
+        
+            foreach ($parameter in $parameters) {
+                $parameterName = $parameter.Name
+                $parameterHelp = $Help.parameters.parameter | Where-Object Name -EQ $parameterName
+            
+                # Should be a description for every parameter
+                It "gets help for parameter: $parameterName : in $commandName" {
+                    $parameterHelp.Description.Text | Should Not BeNullOrEmpty
+                }
+            
+                # Required value in Help should match IsMandatory property of parameter
+                It "help for $parameterName parameter in $commandName has correct Mandatory value" {
+                    $codeMandatory = $parameter.IsMandatory.toString()
+                    $parameterHelp.Required | Should Be $codeMandatory
+                }
+            
+                # Parameter type in Help should match code
+                It "help for $commandName has correct parameter type for $parameterName" {
+                    $codeType = $parameter.ParameterType.Name
+                    # To avoid calling Trim method on a null object.
+                    $helpType = if ($parameterHelp.parameterValue) { $parameterHelp.parameterValue.Trim() }
+                    $helpType | Should be $codeType
+                }
+            }
+        
+            foreach ($helpParm in $HelpParameterNames) {
+                # Shouldn't find extra parameters in help.
+                It "finds help parameter in code: $helpParm" {
+                    $helpParm -in $parameterNames | Should Be $true
+                }
+            }
+        }
     }
 }
