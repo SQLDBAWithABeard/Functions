b2b0d18b87a3f22fd8e3a9766547f99dd43a655d
Rob Sewell <mrrobsewell@outlook.com>
2019-06-17 07:57:20 +0100

    Create Convert-ADSPowerShellForMarkdown.ps1

---
+++ Convert-ADSPowerShellForMarkdown.ps1
@@ -0,0 +1,43 @@
+function Convert-ADSPowerShellForMarkdown {
+    <#
+    .SYNOPSIS
+    Converts URL encoded PowerShell code from https://www.url-encode-decode.com/ into valid Markdown URI
+    
+    .DESCRIPTION
+    Converts URL encoded PowerShell code from https://www.url-encode-decode.com/ into valid Markdown URI
+    
+    .PARAMETER inputstring
+    The endoded URL from the website
+    
+    .PARAMETER ToClipBoard
+    Will not output to screen but instead will set the clipboard
+    
+    .EXAMPLE
+    Convert-ADSPowerShellForMarkdown if+%28-not+%28%24IsLinux+-or+%24IsMacOS%29+%29+%7B%0D%0A++++if+%28-not%28Test-Path+C%3A%5C%5CMSSQL%5C%5CBACKUP%29%29+%7B%0D%0A++++++++Write-Output+%22I%27m+going+to+create+C%3A%5C%5CMSSQL%5C%5CBACKUPS+so+that+the+docker-compose+will+work%22%0D%0A++++++++New-Item+C%3A%5CMSSQL1%5CBACKUP1+-ItemType+Directory%0D%0A++++%7D%0D%0A++++else+%7B%0D%0A++++++++Write-Output+%22C%3A%5C%5CMSSQL%5C%5CBACKUPS+already+exists%22%0D%0A++++%7D%0D%0A%7D%0D%0Aelse+%7B%0D%0A++++Write-Warning+%22Sorry+This+code+won%27t+run+on+Linux+-+You+will+have+to+do+it+manually+and+edit+the+docker+compose+file%22%0D%0A%7D -ToClipBoard
+
+    Converts the encoded URL so that it works with MarkDown and sets it to the clipboard
+    
+    .NOTES
+    June 2019 - Rob Sewell @SQLDbaWithBeard
+    SQLDBAWithABeard.Com
+    #>
+    
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string]$inputstring,
+        [switch]$ToClipBoard
+    )
+    clear
+    $outputstring = $inputstring.Replace('+',' ').Replace('%3A',':').Replace('%5C','%5C%5C').Replace('%22','\u0022').Replace('%27','\u0027').Replace('%0D%0A','')
+    if($ToClipBoard){
+        if (-not ($IsLinux -or $IsMacOS) ) {
+        $outputstring | Set-Clipboard
+        }
+        else{
+            Write-Warning "Set-Clipboard - Doesnt work on Linux - Outputting to screen" 
+            $outputstring
+        }
+    }else {
+        $outputstring
+    }
+}
